cb7b3058e2134413337cfa3170860426399227aa
anjaeseong <anjaeseong@test.com>
2021-05-14 17:31:22 +0900

    8교시 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e12808ade8eb1cd7e83c0",
+      "age": 21,
+      "eyeColor": "brown",
+      "name": "Lydia Sims"
+    },
+    {
+      "_id": "609e12803719a474dfea857c",
+      "age": 33,
+      "eyeColor": "brown",
+      "name": "Carolyn Lowery"
+    },
+    {
+      "_id": "609e128093fc9cd56f5a04c1",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Haney Newton"
+    },
+    {
+      "_id": "609e1280b76382ebb6309e35",
+      "age": 36,
+      "eyeColor": "blue",
+      "name": "Sheppard Mckinney"
+    },
+    {
+      "_id": "609e128087d65fba2e05360c",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Kirkland York"
+    },
+    {
+      "_id": "609e12802e4736e79859ecab",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Elaine Snow"
+    },
+    {
+      "_id": "609e12802a8de99d1777b6e6",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Espinoza Cortez"
+    },
+    {
+      "_id": "609e1280ebcae22d8edf87a5",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Bridgett Jimenez"
+    },
+    {
+      "_id": "609e12804382a97d90166672",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Zamora Coffey"
+    },
+    {
+      "_id": "609e1280f01067ba4e4ef851",
+      "age": 27,
+      "eyeColor": "blue",
+      "name": "Caldwell Hubbard"
+    },
+    {
+      "_id": "609e12808e6d12650a8662c9",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Monique Blackburn"
+    },
+    {
+      "_id": "609e12808588d15c079a4c56",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Lorna Dean"
+    },
+    {
+      "_id": "609e128086fad185654af23f",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Bailey King"
+    },
+    {
+      "_id": "609e12800ad9f54a95f4da6c",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Ballard James"
+    },
+    {
+      "_id": "609e1280ae168b0066fc3df7",
+      "age": 28,
+      "eyeColor": "blue",
+      "name": "Zimmerman Adams"
+    },
+    {
+      "_id": "609e128056f6b68a42ec2e2f",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Burks Mccray"
+    },
+    {
+      "_id": "609e1280dcfa9167bc14e5fa",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "May Joyce"
+    },
+    {
+      "_id": "609e1280f46077e1b7490711",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Sherry Humphrey"
+    },
+    {
+      "_id": "609e128029b6d1c2808f3afb",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Althea Fitzgerald"
+    },
+    {
+      "_id": "609e1280371997c5ea2b55b1",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": "Patricia Simpson"
+    }
+  ]
